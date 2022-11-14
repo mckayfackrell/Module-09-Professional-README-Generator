879a36dacdb194d7ba7bc0f5729558616c0c8656
“mckayfackrell” <mvfackrell@gmail.com>
2022-11-14 12:35:32 -0700

    added options for licenses

--- Develop/index.js
+++ Develop/index.js
@@ -71,15 +71,21 @@ const questions = [
     type: "list",
     name: "license",
     message: "describe what license your application uses",
-    validate: (licenseInput) => {
-      if (licenseInput) {
-        return true;
-      } else {
-        console.log("Please describe what license your application uses");
-        return false;
-      }
+    choices: [
+      'None',
+      'Apache License 2.0',
+      'GNU General Public License v3.0',
+      'MIT License',
+      'BSD 2-Clause "Simplified" License',
+      'BSD 3-Clause "New" or "Revised" License',
+      'Boost Software License 1.0',
+      'Creative Commons Zero v1.0 Universal',
+      'Eclipse Public License 2.0',
+      'GNU Affero General Public License v3.0',
+      'GNU General Public License v2.0',
+      'GNU Lesser General Public License v2.1'
+    ]
     },
-  },
   {
     type: "input",
     name: "contribution",
